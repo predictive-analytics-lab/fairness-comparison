3e5d721e51e327aa415b516eb603bae172133fc9
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-04-25 18:59:17 +0100

    Enable fair GP
    This adds lots of code for preparing the data, choosing the right
    parameters and so on.

--- algorithms/universalgp/UniversalGPAlgorithm.py
+++ algorithms/universalgp/UniversalGPAlgorithm.py
@@ -8,6 +8,8 @@ from universalgp.datasets.definition import Dataset, to_tf_dataset_fn
 from ..Algorithm import Algorithm
 
 USE_EAGER = False
+DO_FAIR = True
+MAX_NUM_INDUCING = 500
 
 
 class UniversalGPAlgorithm(Algorithm):
@@ -19,7 +21,8 @@ class UniversalGPAlgorithm(Algorithm):
         Algorithm.__init__(self)
         self.name = 'UniversalGP'
 
-    def run(self, train_df, test_df, class_attr, positive_class_val, sensitive_attrs, single_sensitive, privileged_vals,
+    @staticmethod
+    def run(train_df, test_df, class_attr, positive_class_val, sensitive_attrs, single_sensitive, privileged_vals,
             params):
         """
         Runs the algorithm and returns the predicted classifications on the test set.  The given train and test data
@@ -51,19 +54,28 @@ class UniversalGPAlgorithm(Algorithm):
         test_label = test_df[class_attr].values[:, np.newaxis]
         test_df_nosensitive = test_df.drop(columns=sensitive_attrs).drop(columns=class_attr)
         test_nosensitive = test_df_nosensitive.values
+        num_train = train_nosensitive.shape[0]
+        num_inducing = min(num_train, MAX_NUM_INDUCING)
+        assert list(np.unique(train_sensitive)) == [0, 1] or list(np.unique(train_sensitive)) == [0., 1.]
+
+        if params['s_as_input']:
+            inducing_inputs = np.concatenate((train_nosensitive[::num_train // num_inducing],
+                                              train_sensitive[::num_train // num_inducing]), -1)
+        else:
+            inducing_inputs = train_nosensitive[::num_train // num_inducing]
 
         dataset = Dataset(
             train_fn=to_tf_dataset_fn(train_nosensitive, train_label, train_sensitive),
             test_fn=to_tf_dataset_fn(test_nosensitive, test_label, test_sensitive),
-            input_dim=train_nosensitive.shape[1],
+            input_dim=train_nosensitive.shape[1] + 1 if params['s_as_input'] else train_nosensitive.shape[1],
             # xtrain=train_nosensitive,
             # ytrain=train_label,
             # strain=train_sensitive,
             # xtest=test_nosensitive,
             # ytest=test_label,
             # stest=test_sensitive,
-            num_train=train_nosensitive.shape[0],
-            inducing_inputs=train_nosensitive,
+            num_train=num_train,
+            inducing_inputs=inducing_inputs,
             output_dim=train_label.shape[1],
             lik="LikelihoodLogistic",
             metric=""
@@ -79,38 +91,47 @@ class UniversalGPAlgorithm(Algorithm):
             tf.logging.set_verbosity(tf.logging.INFO)
             train_func = ugp.train_graph.train_gp
 
+        biased_acceptance1, biased_acceptance2 = UniversalGPAlgorithm._compute_bias(train_label, train_sensitive)
+
         gp = train_func(dataset, dict(
-            inf='Variational',
+            inf='VariationalYbar' if DO_FAIR else 'Variational',
             cov='SquaredExponential',
             lr=0.005,
             loo_steps=None,
             model_name='local',
             batch_size=50,
-            train_steps=50,
+            train_steps=1000,
             eval_epochs=10000,
-            summary_steps=100,
+            summary_steps=5000,
             chkpnt_steps=5000,
             save_dir=None,
             plot=None,
-            logging_steps=1,
+            logging_steps=100,
             gpus='0',
             save_preds=False,
             num_components=1,
-            num_samples=100,
+            num_samples=1000,
             diag_post=False,
             optimize_inducing=True,
             use_loo=False,
             length_scale=1.0,
             sf=1.0,
             iso=False,
-            num_samples_pred=200
+            num_samples_pred=2000,
+            target_rate1=.5 * (biased_acceptance1 + biased_acceptance2),
+            target_rate2=.5 * (biased_acceptance1 + biased_acceptance2),
+            biased_acceptance1=biased_acceptance1,
+            biased_acceptance2=biased_acceptance2,
+            s_as_input=params['s_as_input'],
+            probs_from_flipped=False,
         ))
 
         if USE_EAGER:
             pred_mean, _ = gp.predict({'input': test_nosensitive})
             pred_mean = pred_mean.numpy()
         else:
-            predictions_gen = gp.predict(lambda: to_tf_dataset_fn(test_nosensitive, test_label)().batch(50))
+            predictions_gen = gp.predict(lambda: to_tf_dataset_fn(
+                test_nosensitive, test_label, test_sensitive)().batch(50))
             pred_mean = []
             # pred_var = []
             for prediction in predictions_gen:
@@ -118,17 +139,25 @@ class UniversalGPAlgorithm(Algorithm):
                 # pred_var.append(prediction['var'])
             pred_mean = np.stack(pred_mean)
             # pred_var = np.stack(pred_var)
-        return (pred_mean > 0.5).astype(np.float), []
+        return (pred_mean > 0.5).astype(test_label.dtype), []
 
-    def get_param_info(self):
+    @staticmethod
+    def _compute_bias(labels, sensitive):
+        rate_y1_s0 = np.sum(labels[sensitive == 0] == 1) / np.sum(sensitive == 0)
+        rate_y1_s1 = np.sum(labels[sensitive == 1] == 1) / np.sum(sensitive == 1)
+        return rate_y1_s0, rate_y1_s1
+
+    @staticmethod
+    def get_param_info():
         """
         Returns a dictionary mapping algorithm parameter names to a list of parameter values to be explored. This
         function should only be implemented if the algorithm has specific parameters that should be tuned, e.g., for
         trading off between fairness and accuracy.
         """
-        return {}
+        return dict(s_as_input=[True, False])
 
-    def get_supported_data_types(self):
+    @staticmethod
+    def get_supported_data_types():
 
         return set(["numerical-binsensitive"])
 
@@ -141,9 +170,10 @@ class UniversalGPAlgorithm(Algorithm):
         """
         return self.name
 
-    def get_default_params(self):
+    @staticmethod
+    def get_default_params():
         """
         Returns a dictionary mapping from parameter names to default values that should be used with the algorithm. If
         not implemented by a specific algorithm, this returns the empty dictionary.
         """
-        return {}
+        return dict(s_as_input=True)
