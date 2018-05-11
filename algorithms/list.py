4adaa217a588ed836d10fef9b03bb75921b0742a
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-05-11 11:47:59 +0000

    Settings for benchmarking equality of opportunity

--- algorithms/list.py
+++ algorithms/list.py
@@ -15,17 +15,23 @@ from metrics.DIAvgAll import DIAvgAll
 from metrics.Accuracy import Accuracy
 from metrics.MCC import MCC
 
-ALGORITHMS = [FairGPAlgorithm(s_as_input=True),
-              SVM(),
-              GaussianNB(),
-              LogisticRegression(),
-              DecisionTree(),
-              CaldersAlgorithm(),
-              KamishimaAlgorithm(),
-              FeldmanAlgorithm(SVM()), FeldmanAlgorithm(GaussianNB()),       # Feldman
-              FeldmanAlgorithm(LogisticRegression()), FeldmanAlgorithm(DecisionTree()),
-              ZafarAlgorithmFairness(), ZafarAlgorithmAccuracy(),
-              ZafarEOAlgorithmOpp(), ZafarEOAlgorithmOdd()]
+ALGORITHMS = [
+    UGP(s_as_input=True),
+    UGP(s_as_input=False),
+    UGPEqOpp(s_as_input=True),
+    UGPEqOpp(s_as_input=False),
+    SVM(),
+    # GaussianNB(),
+    # LogisticRegression(),
+    # DecisionTree(),
+    # CaldersAlgorithm(),
+    # KamishimaAlgorithm(),
+    # FeldmanAlgorithm(SVM()), FeldmanAlgorithm(GaussianNB()),       # Feldman
+    # FeldmanAlgorithm(LogisticRegression()), FeldmanAlgorithm(DecisionTree()),
+    # ZafarAlgorithmFairness(), ZafarAlgorithmAccuracy(),
+    ZafarEOAlgorithmOpp(),
+    # ZafarEOAlgorithmOdd(),
+]
 
 # ALGORITHMS = [UniversalGPAlgorithm(s_as_input=True)]     # baseline
 
@@ -43,4 +49,3 @@ ALGORITHMS = [FairGPAlgorithm(s_as_input=True),
 #    ParamGridSearch(FeldmanAlgorithm(SVM()), Accuracy()),
 #    ParamGridSearch(FeldmanAlgorithm(GaussianNB()), DIAvgAll()),
 #    ParamGridSearch(FeldmanAlgorithm(GaussianNB()), Accuracy())
-
