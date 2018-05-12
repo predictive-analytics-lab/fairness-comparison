171c482eedbf02fd695ba5cea0241a0de7bfc9ee
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-05-12 18:44:52 +0100

    Replace ggplot in python with matplotlib

--- analysis.py
+++ analysis.py
@@ -1,9 +1,10 @@
+"""Generate figures"""
+
+import subprocess
+from pathlib import Path
 import fire
 import pandas as pd
-from pathlib import Path
-import subprocess
 
-from ggplot import scale_color_manual, ggplot, aes, geom_point, ggtitle
 from matplotlib import pyplot as plt
 
 from data.objects.list import DATASETS, get_dataset_names
@@ -13,9 +14,9 @@ from data.objects.ProcessedData import TAGS
 GRAPHS = [('DIbinary', 'accuracy'), ('sex-TPR', 'sex-calibration-')]
 
 
-def run(dataset=get_dataset_names(), graphs=GRAPHS):
+def run(rfigures=True, dataset=get_dataset_names(), graphs=GRAPHS):
     for dataset_obj in DATASETS:
-        if not dataset_obj.get_dataset_name() in dataset:
+        if dataset_obj.get_dataset_name() not in dataset:
             continue
 
         print("\nGenerating graphs for dataset:" + dataset_obj.get_dataset_name())
@@ -24,8 +25,9 @@ def run(dataset=get_dataset_names(), graphs=GRAPHS):
                 print("    type:" + tag)
                 filename = dataset_obj.get_results_filename(sensitive, tag)
                 make_all_graphs(filename, graphs)
-    print("Generating additional figures in R...")
-    subprocess.run(["Rscript", "results/generate-report.R"])
+    if rfigures:
+        print("Generating additional figures in R...")
+        generate_rmd_output()
 
 
 def make_all_graphs(filename, graphs):
@@ -47,8 +49,8 @@ def make_all_graphs(filename, graphs):
 def all_possible_graphs(f):
     graphs = []
     measures = list(f.columns.values)[2:]
-    for i, m1 in enumerate(measures):
-        for j, m2 in enumerate(measures):
+    for m1 in measures:
+        for m2 in measures:
             graphs.append((m1, m2))
     return graphs
 
@@ -85,23 +87,18 @@ def generate_graph(f, xaxis_measure, yaxis_measure, title):
                   "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94",
                   "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d",
                   "#17becf", "#9edae5"]
-        scale = scale_color_manual(values=colors)
-        p = (ggplot(f, aes(x=xaxis_measure, y=yaxis_measure, colour='algorithm')) +
-             geom_point(size=50) + ggtitle(title) + scale)
-        # import ipdb; ipdb.set_trace()
-        plt.style.use('ggplot')
+
+        plt.style.use('ggplot')  # optional
         fig, plot = plt.subplots(figsize=(20, 6))
         for i, (name, group) in enumerate(f.groupby('algorithm')):
             plot.plot(group[xaxis_measure], group[yaxis_measure], 'o', label=name, c=colors[i])
         plot.set_xlabel(xaxis_measure)
         plot.set_ylabel(yaxis_measure)
         plot.set_title(title)
-        plot.legend()
-        fig.savefig(str(save_path / Path(f"pyplot-{xaxis_measure}-{yaxis_measure}.png")))
+        lgd = plot.legend(loc='upper left', bbox_to_anchor=(1, 1))
+        figure_path = str(save_path / Path(f"{xaxis_measure}-{yaxis_measure}.png"))
+        fig.savefig(figure_path, bbox_extra_artists=(lgd,), bbox_inches='tight')
         print(xaxis_measure, yaxis_measure)
-        p.save('results/analysis/%s/%s-%s.png' % (title, xaxis_measure, yaxis_measure),
-               width=20,
-               height=6)
 
 
 def generate_rmd_output():
