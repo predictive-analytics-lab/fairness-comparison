78ce7dea54b78ba48c71a0e4a07ed896191b9667
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-05-14 14:58:09 +0100

    Update run_output_to_file.sh

--- run_output_to_file.sh
+++ run_output_to_file.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # this command puts the output into the file "log.txt"
-python benchmark.py 0 \
-	--num-trials 0 \
+python -u benchmark.py 0 \
+	--num-trials 5 \
 	--datasets '["two-gaussians", "ricci", "adult", "german", "propublica-recidivism", "propublica-violent-recidivism"]' \
 	--algorithms '["UGP_in_True"]' \
 	> log.txt 2>&1 &
