ee78099444df6b254b6d52952b6dff2244c20ddb
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-05-16 14:11:31 +0100

    Update run_output_to_file.sh

--- run_output_to_file.sh
+++ run_output_to_file.sh
@@ -6,10 +6,10 @@ python -u benchmark.py 0 \
 	--datasets '["two-gaussians", "ricci", "adult", "german", "propublica-recidivism", "propublica-violent-recidivism"]' \
 	--algorithms '["UGP_in_True"]' \
 	> log.txt 2>&1 &
-# python -u benchmark.py 0 --num-trials 5  --datasets '["two-gaussians"]'  > log0.txt 2>&1 &
-# python -u benchmark.py 1 --num-trials 5  --datasets '["ricci"]'  > log1.txt 2>&1 &
-# python -u benchmark.py 2 --num-trials 5  --datasets '["adult"]'  > log2.txt 2>&1 &
-# python -u benchmark.py 3 --num-trials 5  --datasets '["german"]'  > log3.txt 2>&1 &
-# python -u benchmark.py 4 --num-trials 5  --datasets '["propublica-recidivism"]'  > log4.txt 2>&1 &
-# python -u benchmark.py 5 --num-trials 5  --datasets '["propublica-violent-recidivism"]'  > log5.txt 2>&1 &
-# python -u benchmark.py 6 --num-trials 5  --datasets '["flipped-labels"]'  > log6.txt 2>&1 &
+# python -u benchmark.py 0 --num-trials 5  --datasets '["two-gaussians"]'  > log_two_gauss.txt 2>&1 &
+# python -u benchmark.py 1 --num-trials 5  --datasets '["ricci"]'  > log_ricci.txt 2>&1 &
+# python -u benchmark.py 2 --num-trials 5  --datasets '["adult"]'  > log_adult.txt 2>&1 &
+# python -u benchmark.py 3 --num-trials 5  --datasets '["german"]'  > log_german.txt 2>&1 &
+# python -u benchmark.py 4 --num-trials 5  --datasets '["propublica-recidivism"]'  > log_pp.txt 2>&1 &
+# python -u benchmark.py 5 --num-trials 5  --datasets '["propublica-violent-recidivism"]'  > log_pp_violent.txt 2>&1 &
+# python -u benchmark.py 6 --num-trials 5  --datasets '["flipped-labels"]'  > log_flipped.txt 2>&1 &
